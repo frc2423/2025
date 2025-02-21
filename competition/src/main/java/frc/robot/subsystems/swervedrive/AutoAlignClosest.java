e99f14d241067f6e75e8ce4652c474c4b1a294c7
Ben Skraly <benskraly@gmail.com>
2025-02-21 12:14:11 -0500

    init commit for competition code

---
+++ competition/src/main/java/frc/robot/subsystems/swervedrive/AutoAlignClosest.java
@@ -0,0 +1,46 @@
+
+package frc.robot.subsystems.swervedrive;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.AngleUtils;
+
+public class AutoAlignClosest extends Command {
+    private Pose2d pose;
+    private boolean isRight;
+    private double dist;
+    private SwerveCommands swerveCommands;
+    private SwerveSubsystem swerve;
+
+    public AutoAlignClosest(SwerveSubsystem swerve, SwerveCommands swerveCommands, double dist, boolean isRight) {
+        this.isRight = isRight;
+        this.dist = dist;
+        this.swerve = swerve;
+        this.swerveCommands = swerveCommands;
+    }
+
+    @Override
+    public void initialize() {
+        pose = Vision.getTagPose(swerve.vision.findClosestTagID(swerve.getPose()));
+    }
+
+    @Override
+    public void execute() {
+        Pose2d targetPose = swerveCommands.addScoringOffset(pose, dist, isRight);// .55
+        swerveCommands.actuallyMoveTo(targetPose);
+    }
+
+    @Override
+    public boolean isFinished() {
+        Pose2d targetPose = swerveCommands.addScoringOffset(pose, dist, isRight);// .55
+
+        double xDistance = Math.abs(targetPose.getX() - swerve.getPose().getX());
+        double yDistance = Math.abs(targetPose.getY() - swerve.getPose().getY());
+        boolean isAngleClose = AngleUtils.areAnglesClose(targetPose.getRotation(), swerve.getPose().getRotation(),
+                Rotation2d.fromDegrees(5));
+        return xDistance < 0.0508 &&
+                yDistance < 0.0508 &&
+                isAngleClose;
+    }
+}
