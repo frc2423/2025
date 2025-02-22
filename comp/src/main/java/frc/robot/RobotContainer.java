f77c2f38eeb19a2cdf8c1af81d213dbd82916cd8
amory-kwarqs <amory.kwarqs@gmail.com>
2025-02-22 14:02:44 -0500

    Updated Auto choosing in RobotContainer. Slava Ukraini

--- comp/src/main/java/frc/robot/RobotContainer.java
+++ comp/src/main/java/frc/robot/RobotContainer.java
@@ -280,7 +280,7 @@ public class RobotContainer {
          */
         public Command getAutonomousCommand() {
                 // An example command will be run in autonomous
-                return drivebase.getAutonomousCommand("New Auto");
+                return drivebase.getAutonomousCommand(m_chooser.getSelected());
         }
 
         public void setDriveMode() {
