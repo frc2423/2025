23d50389f4d18d16081ca08b8c4a610fc018bbd9
amorygalili <amorygalili@gmail.com>
2025-03-01 13:33:03 -0500

    Updated climber code; Added periodic + setSetpoint. Still needs testing. Slava Ukraini

--- comp/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ comp/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -10,23 +10,40 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimberSubsystem extends SubsystemBase {
     private SparkMax motor1 = new SparkMax(22, MotorType.kBrushless);
+    private double setpoint = 0;
+    private double speed = .1;
 
     public ClimberSubsystem() {
         motor1.getEncoder().setPosition(0);
         setDefaultCommand(climbStop());
     }
 
-    private void go(double speed) {
-        motor1.set(speed);
+    public void periodic() {
+        if (Math.abs(setpoint - motor1.getEncoder().getPosition()) < .1) {
+            motor1.set(0);
+        } else if (motor1.getEncoder().getPosition() < setpoint) {
+            motor1.set(-speed);
+        } else if (motor1.getEncoder().getPosition() > setpoint) {
+            motor1.set(speed);
+        }
     }
 
-    private void stop() { // for manual control, sick
-        motor1.set(0);
+    private void setSetpoint(double inputposition, double inputspeed) {
+        setpoint = inputposition;
+        speed = inputspeed;
     }
+
+    // private void go(double speed) {
+    // motor1.set(speed);
+    // }
+
+    // private void stop() { // for manual control, sick
+    // motor1.set(0);
+    // }
 
     public Command climb() {
         var command = run(() -> {
-            go(-.1);
+            setSetpoint(0, .1);
         });
         command.setName("Climber going up");
         return command;
@@ -34,14 +51,14 @@ public class ClimberSubsystem extends SubsystemBase {
 
     public Command deClimb() {
         var command = run(() -> {
-            go(.1);
+            setSetpoint(0, .1);
         });
         command.setName("Climber going down");
         return command;
     }
 
     public Command climbStop() {
-        var command = run(() -> stop());
+        var command = run(() -> setSetpoint(motor1.getEncoder().getPosition(), 0));
         command.setName("Climber Stop");
         return command;
     }
@@ -49,6 +66,8 @@ public class ClimberSubsystem extends SubsystemBase {
     @Override
     public void initSendable(SendableBuilder builder) {
         builder.addDoubleProperty("climbPose", () -> motor1.getAbsoluteEncoder().getPosition(), null);
+        builder.addDoubleProperty("climbSetpoint", () -> setpoint, null);
+        builder.addDoubleProperty("climbSpeed", () -> speed, null);
     } // -147.353760 start /
 
 }
