694417581a5019506d20cf91e0db305770337d39
amorygalili <amorygalili@gmail.com>
2025-03-01 10:17:04 -0500

    Updated funnel spin direction

--- comp/src/main/java/frc/robot/subsystems/FunnelSubsystem.java
+++ comp/src/main/java/frc/robot/subsystems/FunnelSubsystem.java
@@ -31,7 +31,7 @@ public class FunnelSubsystem extends SubsystemBase {
 
     public Command spinIn() {
         var command = run(() -> {
-            spinIn(0.5);
+            spinIn(-0.5);
         });
         command.setName("Spin IN (funnel)");
         return command;
