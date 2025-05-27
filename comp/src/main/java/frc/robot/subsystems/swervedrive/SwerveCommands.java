37d1ea691ffd2b6050ad34806aec32b080480dce
amory-kwarqs <amory.kwarqs@gmail.com>
2025-05-27 19:29:57 -0400

    Modified elevator settings for Toyota testing

--- comp/src/main/java/frc/robot/subsystems/swervedrive/SwerveCommands.java
+++ comp/src/main/java/frc/robot/subsystems/swervedrive/SwerveCommands.java
@@ -259,7 +259,7 @@ public class SwerveCommands {
                 intakeCommands.in(),
                 Commands.waitUntil(() -> swerve.getPose().getTranslation().getDistance(
                         (PoseTransformUtils.isRedAlliance()) ? new Translation2d(13.055, 4.007)
-                                : new Translation2d(4.507, 4.031)) < 3.5),
+                                : new Translation2d(4.507, 4.031)) < 2.75),
                 elevatorLevelCommand,
                 Commands.waitUntil(() -> {
                     return elevatorSubsystem.isAtSetpoint();
