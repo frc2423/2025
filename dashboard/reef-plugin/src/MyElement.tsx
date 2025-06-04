75a5b9e7a6db7e99a9fbf42cd9b6566d04a6e392
amory-kwarqs <amory.kwarqs@gmail.com>
2025-06-03 20:59:31 -0400

    Made progress on linking coral game piece buttons to checkboxes, need to fix buttons not turning off/going back to 0 when checkbox is off

--- dashboard/reef-plugin/src/MyElement.tsx
+++ dashboard/reef-plugin/src/MyElement.tsx
@@ -11,44 +11,88 @@ function ReefFace(props: ReefFaceProps) {
   const angle = (props.index*Math.PI/3) + Math.PI / 6;
   const x = 155*Math.cos(angle);
   const y = 155*Math.sin(angle);
-
+  const checkbox_x = 50*Math.cos(angle);
+  const checkbox_y = 50*Math.sin(angle);
   return (
-    <div
-    data-face={props.name}
-    style={{
-      width: "100px",
-      display: "grid",
-      gridTemplateColumns: "repeat(2, 1fr)",
-      gridTemplateRows:
-        "repeat(3, auto)" /* Creates 3 rows with automatic height */,
-      gap: "5px" /* Sets a 5px gap between both rows and columns */,
-      position: "absolute",
-      transform: `translate(${x}px,${y}px) rotate(${angle - Math.PI / 2}rad)`
-    }}
-  >
-    {(props.values ?? []).map((value, index) => {
-      return (
-        <button
+    <div style = {{
+border: "3px solid green"
+    }}>
+         <div style = {{
+            width: "100px",
+            height: "50px",
+            // border: "4px solid pink",
+            display: "flex",
+            justifyContent: "center",
+            gridArea: "header",
+            position: "absolute",
+            top: "78px",
+            transform: `translate(${checkbox_x}px,${checkbox_y}px) rotate(${angle - Math.PI / 2}rad)`
+          }}>
+            <input
+            onClick={(e) => {
+              // for (let value in props.values) {
+              //   if (value == "0") { 
+              //       props.setProperty(props.name, [1, 1, 1, 1, 1, 1]);
+              //       console.log(value);
+              //       return;
+              //   } else {
+              //     console.log("else ran" + props.values);
+              //   }
+              // }
+              // console.log("value is 1");
+              // props.setProperty(props.name, [0, 0, 0, 0, 0, 0]);
+              console.log(e.currentTarget.value)
+
+            }}
+
+
+            style={{
+              width: "25px",
+              height: "25px",
+            }}
+              type="checkbox"/>
+          </div>
+          
+      <div
+      data-face={props.name}
+      style={{
+        width: "100px",
+        display: "grid",
+        gridTemplateColumns: "repeat(2, 1fr)",
+        gridTemplateRows:
+          "repeat(3, auto)" /* Creates 3 rows with automatic height */,
+          gap: "5px" /* Sets a 5px gap between both rows and columns */,
+          position: "absolute",
+          transform: `translate(${x}px,${y}px) rotate(${angle - Math.PI / 2}rad)`,
+          // gridTemplateAreas:
+          //   "header header"
+      }}
+    >
+       
+
+      {(props.values ?? []).map((value, index) => {
+        return (
+          <button
           style={{
             width: 50,
             height: 50,
             backgroundColor: value === 0 ? "lightgray" : "lime",
             borderRadius: "50%",
             fontSize: 30,
-            fontFamily: "Copperplate"
-
+            
           }}
           onClick={() => {
-            const newValues = [...props.values];
-            const newValue = 1 - value;
-            newValues[index] = newValue;
-            props.setProperty(props.name, newValues);
-          }}
-        >
-          L{4 - Math.floor(index / 2)}
-        </button>
-      );
-    })}
+              const newValues = [...props.values];
+              const newValue = 1 - value;
+              newValues[index] = newValue;
+              props.setProperty(props.name, newValues);
+            }}
+            >
+            L{4 - Math.floor(index / 2)}
+          </button>
+        );
+      })}
+    </div>
   </div>
   )
 }
